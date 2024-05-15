a0a2bcd7bf7f8f4715384f8491cf74e6508094f6
marco.spasiano <marco.spasiano@cnr.it>
2024-05-15 08:19:10 +0200

    FIX Scheduling and add @EnableScheduling on Spring Boot Application

--- src/main/java/it/cnr/anac/transparency/result/TransparencyResultServiceApplication.java
+++ src/main/java/it/cnr/anac/transparency/result/TransparencyResultServiceApplication.java
@@ -19,8 +19,10 @@ package it.cnr.anac.transparency.result;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class TransparencyResultServiceApplication {
 
